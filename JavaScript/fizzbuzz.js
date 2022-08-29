de9c1a4415cfa76801da0ad5ffded8ed8e1ef049
fatimaafaneh47 <fatima.afaneh.2015@gmail.com>
2022-08-29 11:44:29 +0300

    fizzbuzz

---
+++ JavaScript/fizzbuzz.js
@@ -0,0 +1,15 @@
+var x;
+
+    for(x=1;x<=100;x++){
+    if(x%3==0){
+    console.log("fizz")
+    }
+    else if(x%5==0){
+    console.log("buzz")
+    }
+    else{ (x%3==0)&&(x%5==0)
+    console.log("fizzbuzz")
+    }
+    }
+
+   
