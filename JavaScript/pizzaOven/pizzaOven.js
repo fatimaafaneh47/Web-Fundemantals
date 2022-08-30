2eb70bc96cc9e36ebb6a8f04bdead5065aba3986
fatimaafaneh47 <fatima.afaneh.2015@gmail.com>
2022-08-30 13:10:52 +0300

    edit

--- JavaScript/pizzaOven/pizzaOven.js
+++ JavaScript/pizzaOven/pizzaOven.js
@@ -1,15 +1,15 @@
-function pizzaOven(crustType,sausageType,chesses,toppings){
-    var pizza={};
-    pizza.crustType=crustType;
-    pizza.sauseType=sausageType;
-    pizza.chesses=chesses;
-    pizza.toppings=toppings;
+function pizzaOven(crustType, sausageType, chesses, toppings) {
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauseType = sausageType;
+    pizza.chesses = chesses;
+    pizza.toppings = toppings;
     return pizza;
 }
-var p1=pizzaOven("deep dish","traditional","mozzarella",["pepperoni","sausage"]);
-var p2=pizzaOven("hand tossed","mariana",["mozarella","feta"],["mushrooms","olives","onions"]);
-var p3=pizzaOven("Thin Crust","Smoky Polish ","Goat","Onion");
-var p4=pizzaOven("Cheese Crust","Italian","Ricotta","Black olives");
+var p1 = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+var p2 = pizzaOven("hand tossed", "mariana", ["mozarella", "feta"], ["mushrooms", "olives", "onions"]);
+var p3 = pizzaOven("Thin Crust", "Smoky Polish ", ["Goat"], ["Onion"]);
+var p4 = pizzaOven("Cheese Crust", "Italian", ["Ricotta"], ["Black olives"]);
 console.log(p1);
 console.log(p2);
 console.log(p3);
