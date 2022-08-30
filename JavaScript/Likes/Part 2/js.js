7cb6291aabe3bae19868c54c0d433ffc9b3ddf41
fatimaafaneh47 <fatima.afaneh.2015@gmail.com>
2022-08-30 19:30:21 +0300

    part2

---
+++ JavaScript/Likes/Part 2/js.js
@@ -0,0 +1,19 @@
+var likes =document.querySelector(".numbers");
+var count = 1;
+function incrementButton(){
+    count++;
+    likes.innerText = count + " like(s)";
+}
+var likes1 =document.querySelector(".numbers1");
+var count1 = 1;
+function incrementButton1(){
+    count1++;
+    likes1.innerText = count1 + " like(s)";
+}
+
+var likes2 =document.querySelector(".numbers2");
+var count2 = 1;
+function incrementButton3(){
+    count2++;
+    likes2.innerText = count2 + " like(s)";
+}
